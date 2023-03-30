58b9384ce7b7762f863d69a10ae096074269cc4d
Piorosen <aoikazto@naver.com>
2023-03-30 12:41:05 -0700

    NCNN 관련 개발 마무리하였음.

--- neural_model/onnx2ncnn.sh
+++ neural_model/onnx2ncnn.sh
@@ -8,10 +8,15 @@ mkdir vgg16
 mkdir mobilenet
 mkdir googlenet
 
-python3 -m onnxsim ../onnx/alexnet.onnx ./alexnet.onnx
-python3 -m onnxsim ../onnx/vgg16.onnx ./vgg16.onnx
-python3 -m onnxsim ../onnx/googlenet.onnx ./googlenet.onnx
-python3 -m onnxsim ../onnx/mobilenet.onnx ./mobilenet.onnx
+# python3 -m onnxsim ../onnx/alexnet.onnx ./alexnet.onnx
+# python3 -m onnxsim ../onnx/vgg16.onnx ./vgg16.onnx
+# python3 -m onnxsim ../onnx/googlenet.onnx ./googlenet.onnx
+# python3 -m onnxsim ../onnx/mobilenet.onnx ./mobilenet.onnx
+
+cp ../onnx/alexnet.onnx ./alexnet.onnx
+cp ../onnx/vgg16.onnx ./vgg16.onnx
+cp ../onnx/googlenet.onnx ./googlenet.onnx
+cp ../onnx/mobilenet.onnx ./mobilenet.onnx
 
 ./onnx2ncnn ./alexnet.onnx ./alexnet.param ./alexnet.bin
 ./onnx2ncnn ./vgg16.onnx ./vgg16.param ./vgg16.bin
@@ -24,6 +29,13 @@ python3 -m onnxsim ../onnx/mobilenet.onnx ./mobilenet.onnx
 .\ncnnoptimize.exe .\googlenet.param .\googlenet.bin .\googlenet-opt.param .\googlenet-opt.bin 0
 
 .\ncnn2table.exe alexnet-opt.param alexnet-opt.bin imagelist.txt alexnet.table mean=[104,117,123] norm=[0.017,0.017,0.017] shape=[227,227,3] pixel=BGR thread=8 method=kl
-.\ncnn2table.exe vgg16.param vgg16.bin imagelist.txt vgg16.table mean=[104,117,123] norm=[0.017,0.017,0.017] shape=[224,224,3] pixel=BGR thread=8 method=kl
-.\ncnn2table.exe mobilenet.param mobilenet.bin imagelist.txt mobilenet.table mean=[104,117,123] norm=[0.017,0.017,0.017] shape=[224,224,3] pixel=BGR thread=8 method=kl
-.\ncnn2table.exe googlenet.param googlenet.bin imagelist.txt googlenet.table mean=[104,117,123] norm=[0.017,0.017,0.017] shape=[224,224,3] pixel=BGR thread=8 method=kl
+.\ncnn2table.exe vgg16-opt.param vgg16-opt.bin imagelist.txt vgg16.table mean=[104,117,123] norm=[0.017,0.017,0.017] shape=[224,224,3] pixel=BGR thread=8 method=kl
+.\ncnn2table.exe mobilenet-opt.param mobilenet-opt.bin imagelist.txt mobilenet.table mean=[104,117,123] norm=[0.017,0.017,0.017] shape=[224,224,3] pixel=BGR thread=8 method=kl
+.\ncnn2table.exe googlenet-opt.param googlenet-opt.bin imagelist.txt googlenet.table mean=[104,117,123] norm=[0.017,0.017,0.017] shape=[224,224,3] pixel=BGR thread=8 method=kl
+
+.\ncnn2int8.exe alexnet-opt.param alexnet-opt.bin alexnet-int8.param alexnet-int8.bin alexnet.table
+.\ncnn2int8.exe vgg16-opt.param vgg16-opt.bin vgg16-int8.param vgg16-int8.bin vgg16.table
+.\ncnn2int8.exe mobilenet-opt.param mobilenet-opt.bin mobilenet-int8.param mobilenet-int8.bin mobilenet.table
+.\ncnn2int8.exe googlenet-opt.param googlenet-opt.bin googlenet-int8.param googlenet-int8.bin googlenet.table
+
+
--- neural_model/pytorch_download.py
+++ neural_model/pytorch_download.py
@@ -27,7 +27,7 @@ def convert(model, name, size):
          dummy_input,       # model input (or a tuple for multiple inputs) 
          name + ".onnx",       # where to save the model  
          export_params=True,  # store the trained parameter weights inside the model file 
-         opset_version=10,    # the ONNX version to export the model to 
+         opset_version=14,    # the ONNX version to export the model to 
          do_constant_folding=True,  # whether to execute constant folding for optimization 
          input_names = ['input'],   # the model's input names 
          output_names = ['output'], # the model's output names 
