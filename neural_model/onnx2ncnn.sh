afe40a521a3b06a8aaf1c6e96bd291c57c647db1
Piorosen <aoikazto@naver.com>
2023-03-30 12:07:57 -0700

    중간세이브

--- neural_model/onnx2ncnn.sh
+++ neural_model/onnx2ncnn.sh
@@ -8,13 +8,22 @@ mkdir vgg16
 mkdir mobilenet
 mkdir googlenet
 
-python3 -m onnxsim ../onnx/alexnet.onnx ./alexnet/alexnet.onnx
-python3 -m onnxsim ../onnx/vgg16.onnx ./vgg16/vgg16.onnx
-python3 -m onnxsim ../onnx/googlenet.onnx ./googlenet/googlenet.onnx
-python3 -m onnxsim ../onnx/mobilenet.onnx ./mobilenet/mobilenet.onnx
+python3 -m onnxsim ../onnx/alexnet.onnx ./alexnet.onnx
+python3 -m onnxsim ../onnx/vgg16.onnx ./vgg16.onnx
+python3 -m onnxsim ../onnx/googlenet.onnx ./googlenet.onnx
+python3 -m onnxsim ../onnx/mobilenet.onnx ./mobilenet.onnx
 
-./onnx2ncnn ./alexnet.onnx ./alexnet/alexnet.param ./alexnet/alexnet.bin
-./onnx2ncnn ./vgg16.onnx ./vgg16/vgg16.param ./vgg16/vgg16.bin
-./onnx2ncnn ./mobilenet.onnx ./mobilenet/mobilenet.param ./mobilenet/mobilenet.bin
-./onnx2ncnn ./googlenet.onnx ./googlenet/googlenet.param ./googlenet/googlenet.bin
+./onnx2ncnn ./alexnet.onnx ./alexnet.param ./alexnet.bin
+./onnx2ncnn ./vgg16.onnx ./vgg16.param ./vgg16.bin
+./onnx2ncnn ./mobilenet.onnx ./mobilenet.param ./mobilenet.bin
+./onnx2ncnn ./googlenet.onnx ./googlenet.param ./googlenet.bin
 
+.\ncnnoptimize.exe .\alexnet.param .\alexnet.bin .\alexnet-opt.param .\alexnet-opt.bin 0
+.\ncnnoptimize.exe .\vgg16.param .\vgg16.bin .\vgg16-opt.param .\vgg16-opt.bin 0
+.\ncnnoptimize.exe .\mobilenet.param .\mobilenet.bin .\mobilenet-opt.param .\mobilenet-opt.bin 0
+.\ncnnoptimize.exe .\googlenet.param .\googlenet.bin .\googlenet-opt.param .\googlenet-opt.bin 0
+
+.\ncnn2table.exe alexnet-opt.param alexnet-opt.bin imagelist.txt alexnet.table mean=[104,117,123] norm=[0.017,0.017,0.017] shape=[227,227,3] pixel=BGR thread=8 method=kl
+.\ncnn2table.exe vgg16.param vgg16.bin imagelist.txt vgg16.table mean=[104,117,123] norm=[0.017,0.017,0.017] shape=[224,224,3] pixel=BGR thread=8 method=kl
+.\ncnn2table.exe mobilenet.param mobilenet.bin imagelist.txt mobilenet.table mean=[104,117,123] norm=[0.017,0.017,0.017] shape=[224,224,3] pixel=BGR thread=8 method=kl
+.\ncnn2table.exe googlenet.param googlenet.bin imagelist.txt googlenet.table mean=[104,117,123] norm=[0.017,0.017,0.017] shape=[224,224,3] pixel=BGR thread=8 method=kl
