401a9c4e885ba2cfb1fdd85ea0baa7885da4b294
Piorosen <aoikazto@naver.com>
2023-03-30 23:19:14 -0700

    onnx에서 tflite 변환하는거 쨩어려븜

--- neural_model/onnx2tflite.py
+++ neural_model/onnx2tflite.py
@@ -7,30 +7,30 @@ import os
 import numpy as np
 import tensorflow as tf
 import onnx
-from onnx_tf.backend import prepare
+# from onnx_tf.backend import prepare
 
 # Load the ONNX model   
 
 print(os.getcwd())
 
 def representative_dataset_gen():
-    for _ in range(100):
+    for _ in range(1):
       data = np.random.rand(1, 224, 224, 3)
       yield [data.astype(np.float32)]
       
 # models = ["vgg16",  "mobilenet", "googlenet"]
-models = ["mobilenet"]
+models = ["vgg16"]
 for model_name in models:
-    model = onnx.load(".\\neural_model\\onnx\\" + model_name + ".onnx")
-    # Check that the IR is well formed  
-    onnx.checker.check_model(model)
-    # Print a Human readable representation of the graph
-    onnx.helper.printable_graph(model.graph)
-    tf_rep = prepare(model)
-    tf_rep.export_graph(".\\neural_model\\tf\\" + model_name + ".pb")
+    # model = onnx.load("./neural_model/onnx/" + model_name + ".onnx")
+    # # Check that the IR is well formed  
+    # onnx.checker.check_model(model)
+    # # Print a Human readable representation of the graph
+    # onnx.helper.printable_graph(model.graph)
+    # tf_rep = prepare(model)
+    # tf_rep.export_graph("./neural_model/tf/" + model_name + ".pb")
     # Convert the model
     
-    converter = tf.lite.TFLiteConverter.from_saved_model(".\\neural_model\\tf\\" + model_name + ".pb")
+    converter = tf.lite.TFLiteConverter.from_saved_model("./neural_model/tf/" + model_name + ".pb")
     converter.optimizations = [tf.lite.Optimize.DEFAULT]
     converter.target_spec.supported_ops = [tf.lite.OpsSet.TFLITE_BUILTINS_INT8]
     converter.representative_dataset = representative_dataset_gen
