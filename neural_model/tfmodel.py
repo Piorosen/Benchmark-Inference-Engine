490ad255cd694d8ba9cc82b2b23d01884ebdfedc
Piorosen <aoikazto@naver.com>
2023-03-31 06:46:19 -0700

    alexnet with tfmodel

--- neural_model/tfmodel.py
+++ neural_model/tfmodel.py
@@ -2,22 +2,26 @@ import numpy as np
 import tensorflow as tf 
 import argparse
 import googlenet
+import alexnet
 
 def google():
-    return googlenet.googlenet()
+    return (googlenet.googlenet(), "googlenet")
+
+def alex():
+    return (alexnet.alexnet(), "alexnet")
     
 def vgg16():
-    return tf.keras.applications.vgg16.VGG16(
+    return (tf.keras.applications.vgg16.VGG16(
         include_top=True,
         weights='imagenet',
         input_tensor=None,
         input_shape=None,
         pooling=None,
         classes=1000,
-    )
+    ), "vgg16")
     
 def mobilenetV2():
-    return tf.keras.applications.mobilenet_v2.MobileNetV2(
+    return (tf.keras.applications.mobilenet_v2.MobileNetV2(
         input_shape=(224, 224, 3),
         alpha=1.0,
         include_top=True,
@@ -26,7 +30,7 @@ def mobilenetV2():
         pooling=None,
         classes=1000,
         classifier_activation='softmax',
-    )
+    ), "mobilenetv2")
 
 
 def representative_dataset_gen():
@@ -34,19 +38,25 @@ def representative_dataset_gen():
       data = np.random.rand(1, 224, 224, 3)
       yield [data.astype(np.float32)]
 
-# model = resnet101()
-model = google()
-converter = tf.lite.TFLiteConverter.from_keras_model(model)
-converter.optimizations = [tf.lite.Optimize.DEFAULT]
-converter.target_spec.supported_ops = [tf.lite.OpsSet.TFLITE_BUILTINS_INT8]
-converter.representative_dataset = representative_dataset_gen
-converter.inference_input_type = tf.uint8
-converter.inference_output_type = tf.uint8
-tflite_model = converter.convert()
+models = [mobilenetV2, vgg16, alex, google]
 
-# Save the model.
-with open("google-quant.tflite", 'wb') as f:
-    f.write(tflite_model)
+for m_func in models:
+    model, name = m_func()
 
-model = vgg16()
-# model = mobilenetV2()
+    converter = tf.lite.TFLiteConverter.from_keras_model(model)
+    tflite_model = converter.convert()
+    with open(name + "-.tflite", 'wb') as f:
+        f.write(tflite_model)
+
+    # converter = tf.lite.TFLiteConverter.from_keras_model(model)
+    # converter.optimizations = [tf.lite.Optimize.DEFAULT]
+    # converter.target_spec.supported_ops = [tf.lite.OpsSet.TFLITE_BUILTINS_INT8]
+    # converter.representative_dataset = representative_dataset_gen
+    # converter.inference_input_type = tf.uint8
+    # converter.inference_output_type = tf.uint8
+    # tflite_model = converter.convert()
+
+    # # Save the model.
+    # with open(name + "-quant.tflite", 'wb') as f:
+    #     f.write(tflite_model)
+
