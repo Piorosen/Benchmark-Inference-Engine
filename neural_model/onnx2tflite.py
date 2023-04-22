c992f3c918bdc637b6436493b9a7d9854e934c7a
Piorosen <chacha@udon.party>
2023-04-23 06:54:29 +0900

    Update onnx2tflite.py

--- neural_model/onnx2tflite.py
+++ neural_model/onnx2tflite.py
@@ -5,32 +5,27 @@
 # from onnx_tf.backend import prepare
 import os
 import numpy as np
-import tensorflow.compat.v1 as tf
+import tensorflow as tf
 import onnx
-
 from onnx_tf.backend import prepare
-# from keras_cv_attention_models import model_surgery
-from tensorflow.keras.preprocessing import image
+
+size = 224
 
 # Load the ONNX model   
 def representative_dataset_gen():
+    global size
     for _ in range(1):
-      data = np.random.rand(1, 3, 224, 224)
+      data = np.random.rand(1, 3, size, size)
       yield [data.astype(np.float32)]
     
 
-def onnx2tflite_fp32(model_name):
+def onnx2tflite_fp32(model_name, model_size):
+    global size 
+    size = model_size
+
     model = onnx.load("./neural_model/onnx/" + model_name + ".onnx")
-    # Check that the IR is well formed  
-    # onnx.checker.check_model(model)
-    # Print a Human readable representation of the graph
-    # onnx.helper.printable_graph(model.graph)
     tf_rep = prepare(model)
     tf_rep.export_graph("./neural_model/tf/" + model_name)
-    
-    # Convert the model
-    input_nodes = tf_rep.inputs
-    output_nodes = tf_rep.outputs
 
     converter = tf.lite.TFLiteConverter.from_saved_model("./neural_model/tf/" + model_name)
     converter.optimizations = [tf.lite.Optimize.DEFAULT]
@@ -40,16 +35,19 @@ def onnx2tflite_fp32(model_name):
     with open("./neural_model/tflite/" +model_name + ".tflite", 'wb') as f:
         f.write(tflite_model)
 
-def pb2tflite_int8(model_name):
+def pb2tflite_int8(model_name, model_size):
+    # https://github.com/sithu31296/PyTorch-ONNX-TFLite
+    global size 
+    size = model_size
     converter = tf.lite.TFLiteConverter.from_saved_model("./neural_model/tf/" + model_name)
     converter.optimizations = [tf.lite.Optimize.DEFAULT]
     converter.target_spec.supported_ops = [
         tf.lite.OpsSet.TFLITE_BUILTINS_INT8,
         tf.lite.OpsSet.SELECT_TF_OPS]
     converter.representative_dataset = representative_dataset_gen
-    converter.inference_input_type = tf.uint8
-    converter.inference_output_type = tf.uint8
-    # converter.target_spec.supported_types = [tf.int8]
+    converter.inference_input_type = tf.int8
+    converter.inference_output_type = tf.int8
+
     tflite_model = converter.convert()
-    with open("./neural_model/tflite/" +model_name + "-uint8.tflite", 'wb') as f:
+    with open("./neural_model/tflite/" +model_name + "-int8.tflite", 'wb') as f:
         f.write(tflite_model)
